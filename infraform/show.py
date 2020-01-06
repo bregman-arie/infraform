d46e54c069fd46847edb9ad76763adcb38bbcf14
abregman <abregman@redhat.com>
2020-01-06 15:16:17 +0200

    LOG a case of non-existing scneario when using ifr show

--- infraform/show.py
+++ infraform/show.py
@@ -11,6 +11,7 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+import crayons
 import logging
 import os
 
@@ -22,6 +23,7 @@ SCENARIOS_PATH = os.path.dirname(__file__) + '/../infraform/scenarios'
 
 
 def show_scenario(scenario):
+    found=False
     for (dirpath, dirnames, filenames) in os.walk(SCENARIOS_PATH):
         for f in filenames:
             name = get_match_until_first_dot(f)
@@ -29,3 +31,6 @@ def show_scenario(scenario):
                 scenario_path = dirpath + '/' + f
                 with open(scenario_path, 'r') as f:
                     LOG.info(f.read())
+                    found=True
+    if not found:
+        LOG.warning("Scenario not found: {}".format(crayons.red(scenario)))
